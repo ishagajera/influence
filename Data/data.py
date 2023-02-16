5b97ae3afe832ee7a8b6363cd4bed631f6706b09
kunjgala <67849261+kunjgala@users.noreply.github.com>
2023-02-16 01:36:42 -0800

    Add files via upload

---
+++ Data/data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+read_file = pd.read_csv (r'influencers.txt')
+read_file.to_csv (r'data.csv', index=None)
+df = pd.read_csv('data.csv', delimiter = '\t')
+df = df.iloc[1:]
+df.loc[df['Category'] == 'fasion', 'Category'] = 'fashion'
+df.loc[df['Category'] == 'fashion 0.5', 'Category'] = 'fashion'
+beautyUN=list(df['Username'][df['Category'] == 'beauty'])
+familyUN=list(df['Username'][df['Category'] == 'family'])
+fashionUN=list(df['Username'][df['Category'] == 'fashion'])
+fitnessUN=list(df['Username'][df['Category'] == 'fitness'])
+foodUN=list(df['Username'][df['Category'] == 'food'])
+interiorUN=list(df['Username'][df['Category'] == 'interior'])
+petUN=list(df['Username'][df['Category'] == 'pet'])
+travelUN=list(df['Username'][df['Category'] == 'travel'])
+otherUN=list(df['Username'][df['Category'] == 'other'])
