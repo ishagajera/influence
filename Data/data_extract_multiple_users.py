b48297030174bf73990ded4ad6a0797ec7d318c1
Isha Gajera <53473338+ishagajera@users.noreply.github.com>
2023-03-02 00:25:27 +0000

    deleted unwanted files

--- Data/data_extract_multiple_users.py
+++ Data/data_extract_multiple_users.py
@@ -5,8 +5,8 @@ import data_extract_one_profile as usersdata
 #print(data.dictUN)
 
 for key in categories.dictUN:
-    #if(key != 'beautyUN'):
+
         for j in range(len(categories.dictUN[key])):
-    #print(data.dictUN[key][j])
+
             user = categories.dictUN[key][j]
             usersdata.getData(user)
--- Data/data_extract_one_profile.py
+++ Data/data_extract_one_profile.py
@@ -5,27 +5,29 @@ import csv
 import instaloader
 import random
 from argparse import ArgumentParser
+import os
 
 def getData(entered_name):
+    
     p = ArgumentParser()
     p.add_argument("--no-metadata-json")
     p.add_argument("-F","--fast-update")
     p.add_argument("--latest-stamps")
+    #p.add_argument(50,"--count")
     args = p.parse_args()
-   
+    # entered_name = ""
+    
     bot = instaloader.Instaloader(max_connection_attempts=1,download_videos=False, save_metadata=False, post_metadata_txt_pattern='')
-    #so that bot will only to try to req a resource once and if not found, it wil proceed ahead.
-    bot.load_session_from_file('teaminfluence2023','session-teaminfluence2023')
-    #bot.login("influenzer456","influenzer")
-    #print("hi")
-    # Loading a profile from an Instagram handle
-    # profile = instaloader.Profile.from_username(bot.context, entered_name)
+    bot.load_session_from_file('worlds_influencer','session-worlds_influencer')
+   
     while True:
         try:
             profile = instaloader.Profile.from_username(bot.context, entered_name)
+            # print(profile)
             print("Username: ", profile.username)
-            print("Number of Posts: ", profile.mediacount)
-            posts = instaloader.Profile.from_username(bot.context,entered_name).get_posts()
+            # print("Number of Posts: ", profile.mediacount)
+            posts = profile.get_posts()
+            # print("retireved posts:")
             today = date.today()
             year = int(today.strftime("%Y"))
             month = int(today.strftime("%m"))
@@ -43,29 +45,61 @@ def getData(entered_name):
             # dd/mm/YY  
             SINCE = datetime(sinceyear, sincemonth, day)
             UNTIL = datetime(year, month, day)
+            top_50_posts = 50
+            counter = 0
+            # profile = instaloader.Profile.from_username(bot.context, entered_name)
+            # posts = profile.get_posts()
+            # print("starting to add data in influencer isha .csv")
 
+            
+            with open('Users.csv', 'w', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)    
+                        writer.writerow(["Username","Posts Count", "Followees", "Followers","Verified status","Bio"])
+                        # likes = set()
+                        print("Fetching likes of all posts of profile {}.".format(profile.username)) 
+                
+                       
+                        verified_status  = "Verified" if profile.is_verified == True else "Not Verified"
+                        writer.writerow([entered_name,profile.mediacount,profile.followees,profile.followers,verified_status,profile.biography])
 
-            with open(entered_name+'.csv', 'w', newline='', encoding='utf-8') as file:
-                        writer = csv.writer(file)
-                        posts = instaloader.Profile.from_username(bot.context, entered_name).get_posts()
+          
+            comments_list = []
+            
+            print("data entered in abv csv, now adding to influencername.csv")
+            with open('PostsInformation.csv', 'w', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Username","Caption", "Caption Mentions","Hashtags","Is sponsored","Sponsor users","Location","Likes"])
+                for post in takewhile(lambda p: p.date > SINCE, dropwhile(lambda p: p.date > UNTIL, posts)):
+                            if counter <= top_50_posts:
+                                time.sleep(random.randint(11,22))
+                                bot.download_post(post, entered_name)
+                                comments_user = post.get_comments() 
+                                ctr =0
+                                for itr in comments_user:
+                                    if ctr <= 10:
+                                        comments_list.append(itr.text)
+                                        ctr+=1
+                                    else:
+                                         break
+                                located_at = post.location.name if post.location is not None else "None"
+                                writer.writerow([entered_name,post.caption,post.caption_mentions,post.caption_hashtags,post.is_sponsored,post.sponsor_users,located_at,post.likes])
+                                counter +=1
+                            else:
+                                break
 
-                        for post in takewhile(lambda p: p.date > SINCE, dropwhile(lambda p: p.date > UNTIL, posts)):
-                        #for loop above will run in a time limit of range defined in Since - When
-                        #for post in posts:
-                            #for loop above will run for all posts.
-                            time.sleep(random.randint(11,22))
-                            #making code sleep so that we avoid 429 error: too many requests.
-                            bot.download_post(post, entered_name)
-                            posturl = "https://www.instagram.com/p/"+post.shortcode
-
-                            writer.writerow(["post", post.caption, posturl]) #,  post.url ])
-                            #writing to csv file
-                            print("\n\n")
+            with open('Comments.csv', 'w', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Username","Comments"])
+                for itr in comments_list:
+                    writer.writerow([entered_name,itr])
+                 
             break
-            
-        except Exception:
-           print("Oops!  That was no valid username.  Try again...")
-           break
-            
 
-    
+            
+        except Exception as e:
+           print("Oops!  That was no valid username.  Try again...")
+           print(e)
+           break
+
+            
+# getData('babyloveteam')
